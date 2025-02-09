26e4205b4ce12a206999a37c4a08e15024e791f2
AlulCode45 <alulgans70@gmail.com>
2025-02-09 18:05:10 +0700

    feat: remove sticky filter

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -176,35 +176,6 @@
                         <img src="{{asset('img/bg_cat_shoes.jpg') }}" class="img-fluid" alt="">
                     </div>
                     <!-- /top_banner -->
-                    <div id="stick_here"></div>
-                    <div class="toolbox elemento_stick add_bottom_30">
-                        <div class="container">
-                            <ul class="clearfix">
-                                <li>
-                                    <div class="sort_select">
-                                        <select name="sort" id="sort">
-                                            <option value="popularity" selected="selected">Sort by popularity</option>
-                                            <option value="rating">Sort by average rating</option>
-                                            <option value="date">Sort by newness</option>
-                                            <option value="price">Sort by price: low to high</option>
-                                            <option value="price-desc">Sort by price: high to
-                                        </select>
-                                    </div>
-                                </li>
-                                <li>
-                                    <a href="#0"><i class="ti-view-grid"></i></a>
-                                    <a href="{{ asset('listing-row-1-sidebar-left.html') }}"><i
-                                            class="ti-view-list"></i></a>
-                                </li>
-                                <li>
-                                    <a href="#0" class="open_filters">
-                                        <i class="ti-filter"></i><span>Filters</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- /toolbox -->
                     <div class="row row_item">
                         <div class="col-sm-4">
                             <figure>
